441024c7f6352e6a6c9dd5f82876fed53a1209c7
@algoreadme <dhikaaditya1@gmail.com>
2026-08-31 14:12:40 +0700

    Initial Commit

---
+++ MyLoanly/MyLoanlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoanlyApp.swift
+//  MyLoanly
+//
+//  Created by DHIKA ADITYA ARE on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MyLoanlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyLoanlyTests/MyLoanlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoanlyTests.swift
+//  MyLoanlyTests
+//
+//  Created by DHIKA ADITYA ARE on 31/08/26.
+//
+
+import Testing
+@testable import MyLoanly
+
+struct MyLoanlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
