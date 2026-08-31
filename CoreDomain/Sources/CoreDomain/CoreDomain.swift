9d460e4843612cefdcf60ced62863dd857230cdb
@algoreadme <dhikaaditya1@gmail.com>
2026-08-31 15:02:01 +0700

    feat: modularize architecture by introducing CoreDomain and PackageData Swift packages and integrating them into the main app target

---
+++ CoreDomain/Sources/CoreDomain/CoreDomain.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public class CoreDomain {
+    public init() {}
+    public func greetings() -> String {
+        return "Hello Dhika"
+    }
+}
